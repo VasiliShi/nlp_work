90f991d46b761cd34dd28ec1544fa21630d23440
lynnli <648823000@qq.com>
2021-02-14 17:15:09 +0800

    some commtent

--- pyunit-address/pyunit_address/multitree.py
+++ pyunit-address/pyunit_address/multitree.py
@@ -4,6 +4,7 @@
 # @Author: Jtyoui@qq.com
 # @interpret: 多叉树
 
+# 构建层级结构树的方法，值得借鉴
 
 class MultiTree:
     def __init__(self, value, parent):
@@ -25,12 +26,12 @@ class MultiTree:
         value = values.split(separators)
         cls = self
         while value:
-            v = value.pop(0)
-            for node in cls.children:
+            v = value.pop(0) # 从前向后pop
+            for node in cls.children: # 一个对象，这是一个层次递归的过程；依次找到子节点
                 if node.value == v:
                     cls = node
                     break
-            else:
+            else: # 如果不在子节点中，则创建新的节点
                 new_cls = MultiTree(value=v, parent=cls)
                 cls.add_children(new_cls)
                 if v in ac:
